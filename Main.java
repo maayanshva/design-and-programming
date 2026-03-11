04a904037b2c45467e59016e4a6fd24b716f60ad
maayanshva <maschwar@g.jct.ac.il>
2026-03-11 17:58:52 +0200

    main

--- Main.java
+++ Main.java
@@ -2,16 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public static void main(String[] args)
     {
-        int x = 5;
-        String y= "hello";
 
-        B  b1  =  new B(100);
-        C[]  arrC = new C[3];
-        arrC[0]  =  new B(99);
-        arrC[1]  =  new A();
-        arrC[2]  =  new B(5);
-        arrC[0].f1();
-        arrC[0].f2();
-        ((A)arrC[1]).f3();
     }
 
